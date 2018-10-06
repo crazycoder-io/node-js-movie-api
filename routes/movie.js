1f10fc5752246368e1e0cd97af5f64db1eef42c6
crazycoderTR <kilincaslanmesut@gmail.com>
2018-10-06 18:13:43 +0300

    modified app.js and movie.js

--- routes/movie.js
+++ routes/movie.js
@@ -6,6 +6,66 @@ const Movie = require('../models/model-movie');
 /*---------------------------------------- Variable Definition ----------------------------------------*/
 
 /*----------------------------------------- Control Structure -----------------------------------------*/
+router.get('/', (req, res) => {
+  const promise = Movie.find({});
+  
+  promise.then(data => {
+    res.json(data);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+// Top 10 list
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({}).limit(10).sort({imdb_score: -1});
+
+  promise.then(data => {
+    res.json(data);
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req, res, next) => {
+  const promise = Movie.findById(req.params.movie_id);
+
+  promise.then((data) => {
+    if (!data) 
+      next({message: 'The movie was not found!', code: 1001});
+    
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {new: true /*Parameter used to display updated data*/});
+
+  promise.then((data) => {
+    if (!data) 
+      next({message: 'The movie was not found!', code: 1001});
+    
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+  promise.then((data) => {
+    if (!data) 
+      next({message: 'The movie was not found!', code: 1001});
+    
+    res.json({status: 1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 router.post('/', (req, res, next) => {
   /* ----------------- You can use such -----------------
   const {title, category, country, year, imdb_score} = req.body;
@@ -22,9 +82,23 @@ router.post('/', (req, res, next) => {
   const movie = new Movie(req.body);
   const promise = movie.save();
   
-  promise.then((data) => {
+  promise.then(data => {
     res.json(data);
-  }).catch((err) => {
+  }).catch(err => {
+    res.json(err);
+  });
+});
+
+// Between/:start_year/:end_year 
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find({
+    year: {"$gte": parseInt(start_year) /* $gte: big or equals */, "$lte": parseInt(end_year) /* $lte: small or equals */}
+  });
+
+  promise.then(data => {
+    res.json(data);
+  }).catch(err => {
     res.json(err);
   });
 });
