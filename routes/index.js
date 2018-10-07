af6d1bbafc4f206ed9eb7071237e62a2026120a9
crazycoderTR <kilincaslanmesut@gmail.com>
2018-10-07 23:20:48 +0300

    Modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/model-user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -26,6 +27,41 @@ router.post('/register', (req, res) => {
       res.json(err);
     });
   });  
+});
+
+// Login
+router.post('/authenticate', (req, res) => {
+  const { userName, password } = req.body;
+
+  User.findOne({
+    userName
+  }, (err, data) => {
+    if(err) throw data;
+    if(!data){
+      res.json({
+        status: false,
+        message: 'Authentication failed, user not found!'
+      });
+    }else{
+      bcrypt.compare(password, data.password).then(result => {
+        if(!result){
+          res.json({
+            status: false,
+            message: 'Authentication failed, wrong password'
+          });
+        }else{
+          const payload = {
+            userName
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_keys'), {expiresIn: 720 /* 12 hours */});
+          res.json({
+            status: true,
+            token
+          });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
