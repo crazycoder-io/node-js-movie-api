91a743eaff0b199d201b300745015689acb9ad1d
crazycoderTR <kilincaslanmesut@gmail.com>
2018-10-07 22:30:09 +0300

    Modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
+"use strict";
 const express = require('express');
 const router = express.Router();
+const User = require('../models/model-user');
+const bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
+});
+
+// Register user
+router.post('/register', (req, res) => {
+  const { userName, password } = req.body;
+
+  bcrypt.hash(password, 10 /* default encryption range */).then(hash => {
+    const user = new User({
+      userName,
+      password: hash
+    });
+    
+    const promise = user.save();
+    promise.then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.json(err);
+    });
+  });  
 });
 
 module.exports = router;
