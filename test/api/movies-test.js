1875da6a9111e783d86b71bee090e9dc4e56ca6e
crazycoderTR <kilincaslanmesut@gmail.com>
2018-10-09 00:05:15 +0300

    Added movies-test.js for test

--- test/api/movies-test.js
+++ test/api/movies-test.js
@@ -5,20 +5,22 @@ const should = chai.should();
 const server = require('../../app');
 
 chai.use(chaiHTTP);
-let token;
+let token, movie_id;
 
 // We define the test we will write
-describe('/api/movies test', () => {
+describe('/api/movies tests', () => {
+    // This method' s task bring token
     before((done) => {
         chai.request(server)
             .post('/authenticate')
             .send({ userName: 'crazycoder', password: '12345'})
             .end((err, res) => {
                 token = res.body.token;
-                done();
+                done(); // It means test finished
             });
     });
-    
+
+    // Test method of /GET movies
     describe('/GET movies', () => {
         it('It should GET all the movies', (done) => {
             chai.request(server)
@@ -27,8 +29,59 @@ describe('/api/movies test', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
+                    done(); // It means test finished
                 });
-            done(); // It means test finished
+        });
+    });
+
+    // Test method of /POST movies
+    describe('/POST movies', () => {
+        it('It should POST a movie', (done) => {
+            const movie = {
+                director_id: '5bba0104210f1219b7a6115b',
+                title: 'TEST',
+                category: 'Test codes',
+                country: 'Turkey',
+                year: 2000,
+                imdb_score: 9
+            };
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    movie_id = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    // Test method of
+    describe('/GET/:movie_id movie', () => {
+        it('It should GET a movie by the given id', (done) => {
+            chai.request(server)
+                .get('/api/movies/' + movie_id)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movie_id);
+                    done();
+                });
         });
     });
 });
