e82a17d4d9487c9adb32b840fe26ccf3dcefa7a6
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-02 01:25:44 +0300

    fix test user

--- test/api/movies-test.js
+++ test/api/movies-test.js
@@ -12,7 +12,7 @@ describe('/api/movies tests', () => {
     // This method' s task bring token
     before((done) => {
         chai.request(server)
-            .post('/authenticate/crazycoder/12345')
+            .post('/authenticate', { userName: 'crazycoder', password: 'movieuser' })
             .end((err, res) => {
                 token = res.body.token;
                 done(); 
