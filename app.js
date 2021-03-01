b20c069658f613f66d8f6ee80fda329fb2648f5f
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-02 00:17:11 +0300

    cors implemented

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const movie = require('./routes/movie');
@@ -21,6 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
